6258b985845c3333569b510b3ebb80119f714795
xiaohong <xiaohong@qq.com>
2017-10-10 13:49:22 +0800

    添加java版本

--- jquery-fed-oapage/app.js
+++ jquery-fed-oapage/app.js
@@ -1,5 +1,5 @@
 (function(){
-    var OPENAPIHOST = 'http://30.4.112.39:8080';
+    var OPENAPIHOST = 'http://30.27.108.2:3000';
     var isDingtalk = /DingTalk/.test(navigator.userAgent);
     var proper = {};
     var _userId = '';
@@ -159,7 +159,7 @@
 
         });
         dd.error(function(err){
-            alert(JSON.stringify(err)+"ww");
+            alert(JSON.stringify(err));
         });
         var originalUrl = location.href;
         var corpId = parseCorpId(originalUrl, 'corpId');
@@ -169,7 +169,6 @@
             type: 'GET',
             dataType: 'json',
             success: function(response){
-                alert("123");
                 if (response.errcode === 0){
                     const config = {
                         agentId: response.agentId || '',
@@ -179,15 +178,14 @@
                         signature: response.signature || '',
                         jsApiList: jsApiList || []
                     };
-                
                     dd.config(config);
                     getUserId(corpId);
                 } else {
                     alert(JSON.stringify(response) + 'sign');
                 }
             },
-            error: function(response){
-                alert(response.status+OPENAPIHOST + '/api/jsapi-oauth?href='+encodeURIComponent(location.href));   
+            error: function(){
+
             }
         };
         $.ajax(signRequest);
